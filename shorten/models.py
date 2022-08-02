ea917788a1730a3c1e83f6ba4c1ee80a3bdd374c
Ehsan Barkhordar <barkhordar.me@gmail.com>
2022-08-02 18:40:08 +0430

    make short url even shorter

--- shorten/models.py
+++ shorten/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class ShortUrl(models.Model):
     complete_url = models.URLField(unique=True, db_index=True, )
-    short_url = models.URLField()
+    short_url = models.URLField(unique=True, db_index=True)
--- shorten/views.py
+++ shorten/views.py
@@ -1,18 +1,38 @@
+import logging
+
+from django.db import IntegrityError
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
 
 import hashlib
+import base64
 
 from shorten.models import ShortUrl
 from shorten.serializers import ShortUrlSerializer
+
+logger = logging.getLogger(__name__)
 
 
 class CreateShortUrl(CreateAPIView):
     serializer_class = ShortUrlSerializer
 
     def perform_create(self, serializer):
-        complete_url = serializer.validated_data['complete_url']
-        short_url = "https://bitly/" + hashlib.md5(complete_url.encode('utf-8')).hexdigest()
-        serializer.save(short_url=short_url)
+        hash_size = 8
+        while True:
+            try:
+                complete_url = serializer.validated_data['complete_url']
+                short_url = self.create_short_url(complete_url, hash_size)
+                serializer.save(short_url=short_url)
+                break
+            except IntegrityError:
+                hash_size += 1
+                logger.exception('Short url is not unique')
+
+    @staticmethod
+    def create_short_url(complete_url, hash_size):
+        hasher = hashlib.sha1(complete_url.encode('utf-8'))
+        truncate_hash = base64.urlsafe_b64encode(hasher.digest()[:hash_size]).decode()
+        short_url = "https://bitly/" + truncate_hash
+        return short_url
 
 
 class RetrieveShortUrl(RetrieveAPIView):
